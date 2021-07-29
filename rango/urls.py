74e9a325665a0f6fb423bcbba2725b6c6a9eae22
RunzeLIAO <2556039l@student.gla.ac.uk>
2021-07-29 08:33:07 +0800

    ch6 first commit

--- rango/urls.py
+++ rango/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url
 from django.urls import path
 from rango import views
+
 app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 ]
