77f94b68e7b02d1abbdd3029a546505429e12518
Zhe Miao <2598803M@student.gla.ac.uk>
2021-08-05 21:44:16 +0800

    same as previous step

--- rango/views.py
+++ rango/views.py
@@ -36,19 +36,31 @@ def about(request):
 
 def show_category(request, category_name_slug):
     context_dict= {}
-    try:
-        category = Category.objects.get(slug=category_name_slug)
-        pages = Page.objects.filter(category=category) 
-        context_dict['pages'] = pages
-        context_dict['category'] = category
-        is_new_visit=visitor_cookie_handler(request)
-        if is_new_visit:
-            category.views=category.views+1
-            category.save()
-    except Category.DoesNotExist:
-        context_dict['category'] = None
-        context_dict['pages'] = None
-    
+    if request.method == 'POST':
+        try:
+            keyword = request.POST.get('keyword',None)
+            category = Category.objects.get(slug=category_name_slug)
+            pages = Page.objects.filter(category=category).filter(title__contains=keyword)
+            context_dict['pages'] = pages
+            context_dict['category'] = category
+            context_dict['keyword'] = keyword
+        except Category.DoesNotExist:
+            context_dict['category'] = None
+            context_dict['pages'] = None
+    else:
+        try:
+            category = Category.objects.get(slug=category_name_slug)
+            pages = Page.objects.filter(category=category) 
+            context_dict['pages'] = pages
+            context_dict['category'] = category
+            is_new_visit=visitor_cookie_handler(request)
+            if is_new_visit:
+                category.views=category.views+1
+                category.save()
+        except Category.DoesNotExist:
+            context_dict['category'] = None
+            context_dict['pages'] = None
+        
     return render(request, 'rango/category.html', context=context_dict)
 
 def show_profile(request, username):
